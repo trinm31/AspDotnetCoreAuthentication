b26063e31e4b4d82d407df894a55489c1cfd155c
trinm31 <trinmgcd19833@fpt.edu.vn>
2022-06-18 08:04:42 +0700

    update

--- AuthenticationAspDotnetCore/Controllers/CategoriesController.cs
+++ AuthenticationAspDotnetCore/Controllers/CategoriesController.cs
@@ -5,67 +5,78 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AuthenticationAspDotnetCore.Controllers
-{ 
-    [Authorize]
-   public class CategoriesController : Controller
+{
+    //[Authorize(Roles = SD.Staff-Role)]
+    public class CategoriesController : Controller
     {
         private readonly ApplicationDbContext _db;
+
         public CategoriesController(ApplicationDbContext db)
         {
             _db = db;
         }
-        [Authorize(Roles = "Admin,Staff")]
+
+        // [Authorize(Roles = "Admin,Staff")]
         public IActionResult Index()
         {
+            // get all data and store to list
             var listAllData = _db.Categories.ToList();
             return View(listAllData);
         }
 
-        
-        [Authorize(Roles = "Admin")]
+
+        // [Authorize(Roles = "Admin")]
         [HttpGet]
-        public IActionResult Upsert(int? id)
+        public IActionResult Upsert(int? id) // allow null diền vào cũng dc mà ko cũng dc 
         {
+            // create
             if (id == null)
             {
                 return View(new Category());
             }
 
-            var categories = _db.Categories.Find(id);
+            // update
+            var categories = _db.Categories.Find(id); // xuống db tìm lại category cũ theo id và truyền về view 
             return View(categories);
         }
-        
-       
+
+
         [HttpPost]
-        [Authorize(Roles = "Admin")]
-        public IActionResult Upsert(Category category)
+        // [Authorize(Roles = "Admin")]
+        public IActionResult Upsert(Category category) // nhận vào object category từ view
         {
             if (ModelState.IsValid)
             {
                 if (category.Id == 0)
                 {
+                    // create
                     _db.Categories.Add(category);
-                    _db.SaveChanges();
-                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    // update
+                    _db.Categories.Update(category);
                 }
 
-                _db.Categories.Update(category);
                 _db.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
 
+            // nếu những property không thỏa mãn yêu cầu input thì trả về lại view obj cũ kèm vs lại lỗi
             return View(category);
-
         }
-        
-        
+
+
         [HttpGet]
-        [Authorize(Roles = "Admin")]
+        // [Authorize(Roles = "Admin")]
         public IActionResult Delete(int id)
         {
+            // find category
             var categoryNeedToDelete = _db.Categories.Find(id);
+            // remove and save change
             _db.Categories.Remove(categoryNeedToDelete);
             _db.SaveChanges();
+            // redirect to action index
             return RedirectToAction(nameof(Index));
         }
     }
--- AuthenticationAspDotnetCore/Controllers/ProductsController.cs
+++ AuthenticationAspDotnetCore/Controllers/ProductsController.cs
@@ -27,16 +27,22 @@ namespace AuthenticationAspDotnetCore.Controllers
 
         public IActionResult UpSert(int? id)
         {
+            //1. Create product viewmodel
             ProductVm productVm = new ProductVm();
 
+            //2. Set data for category lít
             productVm.CategoriesList = categoriesSelectListItems(); 
             
+            //3. if create case id == null
             if (id == null)
             {
+                // set product is new obj
                 productVm.Product = new Product();
+                
                 return View(productVm);
             }
-
+            
+            // 4. Update case
             var productFromDb = _db.Products.Find(id);
             productVm.Product = productFromDb;
             return View(productVm);
@@ -45,33 +51,42 @@ namespace AuthenticationAspDotnetCore.Controllers
         [HttpPost]
         public IActionResult UpSert(ProductVm productVm)
         {
+            // 1. Valid Case
             if (ModelState.IsValid)
             {
+                // 1.1 create case
                 if (productVm.Product.Id == 0)
                 {
                     _db.Products.Add(productVm.Product);
                 }
                 else
                 {
+                    // 1.2 update case
                     _db.Products.Update(productVm.Product);
                 }
+                // 1.3 save
                 _db.SaveChanges();
-            
+                // 1.4 đưa về trang index               
                 return RedirectToAction(nameof(Index));
             }
-
+            // 2. Not valid case
+            // 2.1 cấp lại dữ liệu cho categoryList 
             productVm.CategoriesList = categoriesSelectListItems();
-            
+            // 2.2 trả về lại form upsert kèm vs thông tin cũ và lỗi message
             return View(productVm);
         }
 
         [NonAction]
         private IEnumerable<SelectListItem> categoriesSelectListItems()
         {
+            // 1. Get list of categories
             var categories = _db.Categories.ToList();
+            // 2. Cứ mỗi thằng category thì e sẽ tạo ra 1 thằng selectlistitem
             var result = categories.Select(x => new SelectListItem()
             {
+                // hiện thị ra cho người dùng
                 Text = x.Name,
+                // giá trị của sellect item
                 Value = x.Id.ToString()
             });
 
@@ -79,7 +94,7 @@ namespace AuthenticationAspDotnetCore.Controllers
         }
 
         [HttpGet]
-        [Authorize(Roles = "Admin")]
+        // [Authorize(Roles = "Admin")]
         public IActionResult Delete(int id)
         {
             var productNeedToDelete = _db.Products.Find(id);
